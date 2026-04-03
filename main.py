d2bbfbb2761f0313e426ece3f2ea71dd5f3c6bac
Sevinch <ysevinchoy2197@gmail.com>
2026-04-03 15:39:44 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#1-misol
+s1 = lambda a, b: a if a > b else b
+print(s1(10, 9))
+
+#2-misol
+s2 = lambda x: x if x > 0 else 0
+print(s1(-5))
+
+#3-misol
+s3 = lambda a, b, c: a + b + c
+print(s3(3, 4, 5))
+
+#4-misol
+s4 = lambda a, b: a % b
+print(s4(17, 5))
+
+#5-misol
+s5 = lambda x: x * 9/5 + 32
